f746c10a7608e5b07740504b619197501fb7821b
CreamBread868 <tomio.miyazaki@gmail.com>
2025-08-31 19:39:47 +0900

    Function で setupするように

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,11 @@
 import { world ,system} from "@minecraft/server";
-//一番最初だけ実行　setupに使って
 world.afterEvents.playerSpawn.subscribe(ev => {
     if (ev.initialSpawn && !world.getDynamicProperty('iniMessage')) {
         world.sendMessage('オリシスアドオンをダウンロードしていただきありがとうございます。詳しい説明は クラフターズコロニー をご覧ください')
         world.sendMessage('この先の道は苦難が伴うだろう...')
     
         world.setDynamicProperty('iniMessage',true);
-        world.getAllPlayers()[0].runCommand(`gamerule naturalRegeneration false`)
+        world.getAllPlayers()[0].runCommand(`function Osiris/setup`)
     }
 })
 
