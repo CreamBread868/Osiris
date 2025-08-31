2d4f87d39e229b7f894cf5edc93574c582e7484e
CreamBread868 <tomio.miyazaki@gmail.com>
2025-08-31 19:36:48 +0900

    SetUpシステム

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,12 @@
+import { world ,system} from "@minecraft/server";
+//一番最初だけ実行　setupに使って
+world.afterEvents.playerSpawn.subscribe(ev => {
+    if (ev.initialSpawn && !world.getDynamicProperty('iniMessage')) {
+        world.sendMessage('オリシスアドオンをダウンロードしていただきありがとうございます。詳しい説明は クラフターズコロニー をご覧ください')
+        world.sendMessage('この先の道は苦難が伴うだろう...')
+    
+        world.setDynamicProperty('iniMessage',true);
+        world.getAllPlayers()[0].runCommand(`gamerule naturalRegeneration true/false`)
+    }
+})
 
