6303fc3474e324fd51b34e6d17ca6cc7e1b6db35
moaminato <atsumu.moa@gmail.com>
2025-09-27 23:52:35 +0900

    Update main.js

--- behavior/scripts/main.js
+++ behavior/scripts/main.js
@@ -3,6 +3,7 @@ world.afterEvents.playerSpawn.subscribe(ev => {
     if (ev.initialSpawn && !world.getDynamicProperty('iniMessage')) {
         world.sendMessage('オリシスアドオンをダウンロードしていただきありがとうございます。詳しい説明は クラフターズコロニー をご覧ください')
         world.sendMessage('この先の道は苦難が伴うだろう...')
+        world.sendMessage('されど、その先に光はあるのだ')
     
         world.setDynamicProperty('iniMessage',true);
         world.getAllPlayers()[0].runCommand(`function Osiris/setup`)
